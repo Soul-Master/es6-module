556ef361298ad34ca1b8de9e3785ec05c55570a6
Soul_Master <Soul_Master@live.com>
2017-08-17 00:55:59 +0700

    Update TS config to generate JavaScript file

--- mathtimatician.js
+++ mathtimatician.js
@@ -1,15 +1,12 @@
 import defaultExportObj from './math.js';
 import * as mathLib from './math.js';
 import { PI } from './math.js';
-
 // bar
 let obj = new defaultExportObj();
 console.info(obj.foo());
-
 // 3.14159
 console.info(`mathLib.PI: ${mathLib.PI}`);
 console.info(`PI: ${PI}`);
-
 // 20
 let result = mathLib.multiply(5, 4);
 console.info(result);
